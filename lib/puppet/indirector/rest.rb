8b39f63a0a9e67279b41a394bc7bded33db14c32
Chris Price <chris@puppetlabs.com>
2014-11-30 20:45:22 -0800

    (PUP-3644) Add environments endpoint to v3
    This commit adds a copy of the environments endpoint to the v3
    routes.  It includes a bit of refactoring (e.g. creating a new
    class `V3::IndirectedRoutes` to contain the logic for all of the
    routes that come from the indirector) in order to keep the code
    for the newer style routes separate from the code for the indirector
    routes.

--- lib/puppet/indirector/rest.rb
+++ lib/puppet/indirector/rest.rb
@@ -2,12 +2,14 @@ require 'net/http'
 require 'uri'
 
 require 'puppet/network/http'
-require 'puppet/network/http/api/v3'
+require 'puppet/network/http/api/v3/indirected_routes'
 require 'puppet/network/http_pool'
 
 # Access objects via REST
 class Puppet::Indirector::REST < Puppet::Indirector::Terminus
   include Puppet::Network::HTTP::Compression.module
+
+  IndirectedRoutes = Puppet::Network::HTTP::API::V3::IndirectedRoutes
 
   class << self
     attr_reader :server_setting, :port_setting
@@ -83,7 +85,7 @@ class Puppet::Indirector::REST < Puppet::Indirector::Terminus
   end
 
   def find(request)
-    uri, body = Puppet::Network::HTTP::API::V3.request_to_uri_and_body(request)
+    uri, body = IndirectedRoutes.request_to_uri_and_body(request)
     uri_with_query_string = "#{uri}?#{body}"
 
     response = do_request(request) do |request|
@@ -121,7 +123,7 @@ class Puppet::Indirector::REST < Puppet::Indirector::Terminus
 
   def head(request)
     response = do_request(request) do |request|
-      http_head(request, Puppet::Network::HTTP::API::V3.request_to_uri(request), headers)
+      http_head(request, IndirectedRoutes.request_to_uri(request), headers)
     end
 
     if is_http_200?(response)
@@ -133,7 +135,7 @@ class Puppet::Indirector::REST < Puppet::Indirector::Terminus
 
   def search(request)
     response = do_request(request) do |request|
-      http_get(request, Puppet::Network::HTTP::API::V3.request_to_uri(request), headers)
+      http_get(request, IndirectedRoutes.request_to_uri(request), headers)
     end
 
     if is_http_200?(response)
@@ -148,7 +150,7 @@ class Puppet::Indirector::REST < Puppet::Indirector::Terminus
     raise ArgumentError, "DELETE does not accept options" unless request.options.empty?
 
     response = do_request(request) do |request|
-      http_delete(request, Puppet::Network::HTTP::API::V3.request_to_uri(request), headers)
+      http_delete(request, IndirectedRoutes.request_to_uri(request), headers)
     end
 
     if is_http_200?(response)
@@ -163,7 +165,7 @@ class Puppet::Indirector::REST < Puppet::Indirector::Terminus
     raise ArgumentError, "PUT does not accept options" unless request.options.empty?
 
     response = do_request(request) do |request|
-      http_put(request, Puppet::Network::HTTP::API::V3.request_to_uri(request), request.instance.render, headers.merge({ "Content-Type" => request.instance.mime }))
+      http_put(request, IndirectedRoutes.request_to_uri(request), request.instance.render, headers.merge({ "Content-Type" => request.instance.mime }))
     end
 
     if is_http_200?(response)
